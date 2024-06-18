a351ab77ce1f7cb8f50fd5d29422fbea5a98cda7
UnicornDad8 <unicorndad8@gmail.com>
2024-06-18 15:04:43 -0400

    Implemented some of the meeting room functionalities

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import { dark } from "@clerk/themes";
 import "./globals.css";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
