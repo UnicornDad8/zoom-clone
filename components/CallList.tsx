982807c4b36e31ace2f121a8abb3be8147e10d6d
UnicornDad8 <unicorndad8@gmail.com>
2024-06-20 11:18:15 -0400

    Fetching data from useGetCalls hook and added two more functions to CallList component (getCalls, and getNoCallsMessage)

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { useRouter } from "next/navigation";
+import MeetingCard from "./MeetingCard";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
-  return <div>CallList...</div>;
+  const router = useRouter();
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "recordings":
+        return recordings;
+      case "upcoming":
+        return upcomingCalls;
+      default:
+        return [];
+    }
+  };
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case "ended":
+        return "No previous calls";
+      case "recordings":
+        return "No recordings";
+      case "upcoming":
+        return "No upcoming calls";
+      default:
+        return "";
+    }
+  };
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+
+  return (
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? (
+        calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard key={(meeting as Call).id} />
+        ))
+      ) : (
+        <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
+      )}
+    </div>
+  );
 };
 
 export default CallList;
